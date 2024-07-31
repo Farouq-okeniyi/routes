65191b47af846ca88c128df473f7058e846de33d
DESKTOP-1M8BI2T\USER <farouqokeniyi2004@gmail.com>
2024-07-31 12:57:35 +0100

    continuation

--- nexpress/server.js
+++ nexpress/server.js
@@ -1,11 +1,32 @@
 const dotenv = require('dotenv')
+const mongoose = require('mongoose')
 dotenv.config({path: './config.env'});
 const app = require('./app')
 // to know the environment variable
 
     // console.log(app.get('env '))
     console.log(process.env )
-//creating a server
+
+mongoose.connect(process.env.Con_String,{  tls: true, // Enable TLS
+    tlsAllowInvalidCertificates: true,
+
+}).then((conn)=>{
+    console.log(conn);
+    console.log("db connected succesfully");
+}).catch((err)=>{
+    console.log(err)
+})
+
+//schema
+
+const moviesscema = new mongoose.Schema({
+    Name: String,
+    Discription: String,
+    Duration: Number,
+    Rating: Number
+
+})
+    //creating a server
 const port = process.env.PORT || 2000;
 
 
